ace3e7592310e2aea5cc26acfd9096c03af596ed
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-29 12:21:46 +0000

    Improved error reporting by providing detailed error messages instead of failing silently and inexplicably.
    git-svn-id: https://plugins.svn.wordpress.org/link-cloaking-plugin/trunk@390414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wplc_redirector.php
+++ wplc_redirector.php
@@ -4,8 +4,7 @@
 */
 	require_once '../../../wp-load.php';
 	
-	//Set the default redirect URL to use if nothing found
-	$url = get_option('home');
+	$url = false;
 	
 	if( !isset($_GET['name']) ) {
 	
@@ -22,8 +21,33 @@
 				$url = empty($matches[$link_num-1][3])?$matches[$link_num-1][2]:$matches[$link_num-1][3];
 				$url = str_replace('&amp;','&',$url);
 				$url = ltrim($url);
+			} else {
+				wp_die(
+					sprintf(
+						"<h1>Plugin error: Incompatible link</h1>
+						<p>
+						Can't find the correct target URL for this link.
+						
+						Most likely, it's because the link in question was inserted 
+						from a custom field or created using another plugin or theme feature.
+						Those are not supported by the free version of this plugin.
+						</p>",
+						htmlentities($post_id)
+					)			
+				);
 			}
 			
+		} else {
+			wp_die(
+				sprintf(
+					"<h1>Plugin error: Post not found</h1>
+					<p>
+					Can't find the correct target URL for this link.
+					Post %s does not to exist.
+					</p>",
+					htmlentities($post_id)
+				)			
+			);
 		}
 	
 	} else {
@@ -32,15 +56,29 @@
 		$sql="SELECT url FROM $ws_link_cloaker->linkstable_name ".
 			 "WHERE name LIKE '$static_name' LIMIT 1";
 		$url=$wpdb->get_var($sql);
-		if(!$url) { $url=get_option('siteurl'); } else {
+		if($url){ 
 			//Record the hit
 			$wpdb->query("UPDATE $ws_link_cloaker->linkstable_name ".
 						" SET hits=hits+1 WHERE name LIKE '$static_name' LIMIT 1");
+		} else {
+			wp_die(
+				sprintf(
+					"<h1>Plugin error: Link not found</h1>
+					<p>
+					The link \"%s\" is either invalid or has been removed.
+					</p>",
+					htmlentities($static_name)
+				)			
+			);
 		};
 	}
 	
-	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: $url");
-	header("X-Redirect-Src: $ws_link_cloaker->redirector", TRUE);
-	exit();
+	if ( !empty($url) && is_string($url) ){
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: $url");
+		header("X-Redirect-Src: $ws_link_cloaker->redirector", TRUE);
+		exit();
+	} else {
+		wp_die("An unexpected error occured.");
+	}
 ?>
